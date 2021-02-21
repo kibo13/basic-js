fead1505e1530c8e4d3f8a0c1e7dd3c5aa7e8663
Boris Kim <kimboris1310@gmail.com>
2021-02-21 13:53:03 +0500

    feat: complete count-cats task

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,12 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error')
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function countCats(matrix) {
+  let cats = [].concat(...matrix)
+  let count = 0
+
+  for (let cat of cats) {
+    cat == '^^' ? count++ : count
+  }
+
+  return count
+}
