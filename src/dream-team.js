2d42f7074a7fbb4c5514dde4cebfa04fae50a3fe
Boris Kim <kimboris1310@gmail.com>
2021-02-21 14:58:52 +0500

    feat: complete dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error')
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  return Array.isArray(members)
+    ? members
+        .filter((el) => typeof el == 'string')
+        .map((el) => el.trim().charAt(0).toUpperCase())
+        .sort()
+        .join('')
+    : false
+}
