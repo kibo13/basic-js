f610349eb1034e1b3cbd0cd6b094e932644cafc7
Boris Kim <kimboris1310@gmail.com>
2021-02-21 17:58:52 +0500

    feat: complete transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,52 @@
-const CustomError = require("../extensions/custom-error");
+const CustomError = require('../extensions/custom-error')
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+  const discardNext = '--discard-next'
+  const discardPrev = '--discard-prev'
+  const doubleNext = '--double-next'
+  const doublePrev = '--double-prev'
+
+  let newArr = arr.slice()
+
+  for (let i = 0; i < newArr.length; i++) {
+    // the case for '--discard-next'
+    if (newArr[i] == discardNext) {
+      if (i == newArr.length - 1) {
+        newArr[i] = 'deleted'
+      } else {
+        newArr[i] = 'deleted'
+        newArr[i + 1] = 'deleted'
+      }
+    }
+
+    // the case for '--discard-prev'
+    if (newArr[i] == discardPrev) {
+      if (i == 0) {
+        newArr[i] = 'deleted'
+      } else {
+        newArr[i] = 'deleted'
+        newArr[i - 1] = 'deleted'
+      }
+    }
+
+    // the case for '--double-next'
+    if (newArr[i] == doubleNext) {
+      if (i == newArr.length - 1) {
+        newArr[i] = 'deleted'
+      } else {
+        newArr[i] = newArr[i + 1]
+      }
+    }
+
+    // the case for '--double-prev'
+    if (newArr[i] == doublePrev) {
+      if (i == 0) {
+        newArr[i] = 'deleted'
+      } else {
+        newArr[i] = newArr[i - 1]
+      }
+    }
+  }
+
+  return Array.isArray(newArr) ? newArr.filter((el) => el != 'deleted') : false
+}
